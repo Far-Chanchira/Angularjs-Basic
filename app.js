a0ae9e28c59476e0615afe52b2581a0b5018ffd8
chanchira <chanchiray38@gmail.com>
2018-04-05 11:39:45 +0700

    Custom Directive

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ app.controller("myCtrl",function($scope){
   $scope.fName="chanchira";
   $scope.lName="yomchan";
 });
+app.directive("colortext",function(){
+  return {
+    template:"<h1 style='font-family:tahoma;color:pink'>CHANCHIRA<small><br>Angularjs</small></h1>"
+  }
+});
