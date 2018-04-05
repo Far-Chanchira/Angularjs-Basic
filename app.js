7de9f586a0efcdb9ed2655ddc7dcbad70a7bd5db
chanchira <chanchiray38@gmail.com>
2018-04-05 13:43:17 +0700

    Event Click

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var app=angular.module("myApp",[]);
 app.controller("myCtrl",function($scope){
-  $scope.fName="chanchira";
-  $scope.lName="yomchan";
-});
-app.directive("colortext",function(){
-  return {
-    template:"<h1 style='font-family:tahoma;color:pink'>CHANCHIRA<small><br>Angularjs</small></h1>"
+  $scope.count=0;
+  $scope.myFunction=function(){
+    $scope.count++;
   }
 });
